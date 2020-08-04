42e6fe87b7f67fd1262f867552b0c003d544fa41
lehiep071191 <good.boy.0991@gmail.com>
2020-08-04 20:57:55 +0700

    bai tap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  
+  get 'static_pages/home'
+  get 'static_pages/help'
+root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
