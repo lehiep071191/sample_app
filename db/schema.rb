4d55f0a5b061d85ee0a107748e647f9d130dea82
lehiep071191 <good.boy.0991@gmail.com>
2020-08-08 11:11:20 +0700

    chapter9

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_06_023256) do
+ActiveRecord::Schema.define(version: 2020_08_08_032201) do
 
   create_table "books", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_08_06_023256) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
